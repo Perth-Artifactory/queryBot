c714d7a5cf05f92d9c9740af03b7f2e55d92f298
Fletcher <me@fboyd.me>
2023-03-27 23:06:57 +0800

    Add descriptions to events, expand to 20

--- data/events.py
+++ data/events.py
@@ -22,7 +22,7 @@ def pull_events():
         # Call the Calendar API
         now = datetime.utcnow().isoformat() + 'Z'  # 'Z' indicates UTC time
         events_result = service.events().list(calendarId=config["calendar_id"], timeMin=now,
-                                              maxResults=10, singleEvents=True,
+                                              maxResults=20, singleEvents=True,
                                               orderBy='startTime').execute()
         events = events_result.get('items', [])
 
@@ -34,13 +34,20 @@ def pull_events():
         print('An error occurred: %s' % error)
 
 def format_events():
-    s = f'The time right now is {datetime.now()}. These are the next 10 events:'
+    s = f'The time right now is {datetime.now()}. These are the next 20 events:'
+    descriptions = {}
     for event in pull_events():
         f = '%Y-%m-%dT%H:%M:%S+08:00'
         start_dt = datetime.strptime(event["start"]["dateTime"], f)
         end_dt = datetime.strptime(event["end"]["dateTime"], f)
         start_f = "%H:%M on %A %d %B"
         end_f = "%H:%M"
-
         s += f'\n{event["summary"]} starts at {start_dt.strftime(start_f)} until {end_dt.strftime(end_f)}'
+        if event["summary"] not in descriptions:
+            descriptions[event["summary"]] = event["description"]
+    s += "\nThese are a the descriptions for the events listed above."
+    for description in descriptions:
+        s += "\n" + description
+        s += "\n" + descriptions[description]
+        s += "\n------"
     return s
