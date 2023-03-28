4dd9e3b6a58228cdd8d3381a1f6cbe511e5f2d05
Fletcher <me@fboyd.me>
2023-03-28 12:10:21 +0800

    Reduce webpage summary token usage by presummarising

---
+++ data/matryoshka.py
@@ -0,0 +1,97 @@
+import requests
+import json
+import html
+import re
+from pprint import pprint
+import hashlib
+import openai
+
+def download_page(pagedata):
+    p = requests.get(pagedata["download_url"])
+    pagedata["content"] = html.unescape(p.text)
+    titles = re.findall("<title[^>]*>(.*)<\/title>", p.text)
+    if titles:
+        pagedata["title"] = titles[0]
+    return pagedata
+
+def mrkdwn(pagedata):
+    pagedata["title"] = re.findall("\ntitle: '*?(.*)'*?", pagedata["content"])[0].replace("'","")
+    return pagedata
+
+def process_page(url):
+    for block in url_conversions:
+        if block["search"] in url:
+            i = {"url":url,"download_url":url.replace(block["find"],block["replace"])+block["suffix"]}
+
+            for f in block["functions"]:
+                i = f(i)
+            return i
+
+def process_pages():
+    with open("config.json","r") as f:
+        config = json.load(f)
+    urls = config["urls"] 
+
+    out = {}
+
+    for url in urls:
+        out[hashlib.md5(url.encode()).hexdigest()] = process_page(url)
+    return out
+
+def gpt_summarise(pagedata):
+    openai.api_key_path = './key'
+    print(f'initiating summary of {pagedata["title"]}')
+    try:
+        r = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": "system", "content": "You are a helpful worker being run by the Perth Artifactory. You are tasked with summarising the content of web pages in a format that can be used by other iterations of GPT."},
+                {"role": "user", "content": f'This is the contents of the webpage "{pagedata["title"]}"\n{pagedata["content"]}'}
+                ]
+            )
+    except openai.error.InvalidRequestError:
+        pagedata["summary"] = " "
+        return pagedata
+    pagedata["summary"] = r["choices"][0]["message"]["content"]
+    return pagedata
+
+def format_pages(pages):
+    prompts = []
+    for k in pages:
+        pagedata = pages[k]
+        prompts.append({"role": "user", "content": f'There is a webpage titled {pagedata["title"]} at {pagedata["url"]} This is a summary of the page written by a version of GPT:\n{pagedata["summary"]}'})
+    return prompts
+
+url_conversions = [{"search":"wiki.artifactory.org.au/en/",
+                   "find":"wiki.artifactory.org.au/en/",
+                   "replace":"raw.githubusercontent.com/Perth-Artifactory/wiki/main/",
+                   "suffix":".md",
+                   "functions":[download_page,mrkdwn]},
+                  {"search":"artifactory.org.au/pages/",
+                   "find":"artifactory.org.au/pages/",
+                   "replace":"raw.githubusercontent.com/Perth-Artifactory/website/master/_pages/",
+                   "suffix":".md",
+                   "functions":[download_page,mrkdwn]},
+                   ]
+
+data_new = process_pages()
+try:
+    with open("babushka.json","r") as f:
+        try:
+            data_old = json.load(f)
+        except:
+            data_old = {}
+except FileNotFoundError:
+    data_old = {}
+data_final = {}
+
+for d in data_new:
+    if d not in data_old:
+        data_final[d] = gpt_summarise(data_new[d])
+    elif data_new[d]["content"] != data_old[d]["content"]:
+        data_final[d] = gpt_summarise(data_new[d])
+    else:
+        data_final[d] = data_old[d]
+
+with open("babushka.json","w") as f:
+    json.dump(data_final, f, indent=2)
--- gpt.py
+++ gpt.py
@@ -2,15 +2,17 @@ import openai
 import json
 
 from data import events
-from data import wiki
 
 openai.api_key_path = './key'
 
 with open("data.json","r") as f:
     data = json.load(f)
 
-# prefetch wiki pages
-wiki_pages = wiki.format_pages()
+# prefetch internet pages
+from data import matryoshka
+with open("babushka.json","r") as f:
+    webpages = json.load(f)
+    pages = matryoshka.format_pages(webpages)
 
 def respond(prompts):
     try:
@@ -19,11 +21,9 @@ def respond(prompts):
             messages=[
                 {"role": "system", "content": "You are a helpful assistant tasked with drafting replies to questions from the public on behalf of the Perth Artifactory. You are receiving requests from a channel on the artifactory slack team and your name is QueryBot."},
                 {"role": "user", "content": data["bot primer"]},
-                {"role": "user", "content": data["first visit"]},
-                {"role": "user", "content": data["membership"]},
                 {"role": "user", "content": data["workshop usage"]},
                 {"role": "user", "content": events.format_events()}
-                ]+wiki_pages+prompts
+                ]+pages+prompts
             )
     except openai.error.InvalidRequestError:
         return "This has gotten a bit complex for my brain to handle sorry :("
