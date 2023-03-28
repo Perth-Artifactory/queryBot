86942112c50388ec3662c71daf2c05ee11b42968
Fletcher <me@fboyd.me>
2023-03-28 12:35:03 +0800

    Add request for clarification

--- gpt.py
+++ gpt.py
@@ -19,7 +19,7 @@ def respond(prompts):
         r = openai.ChatCompletion.create(
             model="gpt-3.5-turbo",
             messages=[
-                {"role": "system", "content": "You are a helpful assistant tasked with drafting replies to questions from the public on behalf of the Perth Artifactory. You are receiving requests from a channel on the artifactory slack team and your name is QueryBot."},
+                {"role": "system", "content": "You are a helpful assistant tasked with drafting replies to questions from the public on behalf of the Perth Artifactory. You are receiving requests from a channel on the artifactory slack team and your name is QueryBot. If you are ever unsure of information, ask for clarification."},
                 {"role": "user", "content": data["bot primer"]},
                 {"role": "user", "content": data["workshop usage"]},
                 {"role": "user", "content": events.format_events()}
